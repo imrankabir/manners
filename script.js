41075e9d5505cf17795f62afca08bb93cd2e92c2
Imran Kabir <imran.kabir.k@gmail.com>
2024-12-11 21:13:59 +0500

    manners auto commit on 2024-12-11 09:13:59 PM

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ const container = document.querySelector('.container');
 const get = (k, d) => JSON.parse(localStorage.getItem(`manners-${k}`)) ?? d;
 const set = (k, v) => localStorage.setItem(`manners-${k}`, JSON.stringify(v));
 
-const changeOption = e => {
-    const option = e.target.value;
-    console.log({option});
-    set('option', {option});
-    markOptionSelected(option);
-};
-
 const markOptionSelected = option => {
     document.querySelectorAll('.option').forEach(option => option.classList.add('hide'));
     document.querySelector(`.option-${option}`).classList.remove('hide');
 };
 
-options.forEach(option => option.addEventListener('click', changeOption));
+options.forEach(option => option.addEventListener('click', e => {
+    const option = e.target.value;
+    set('option', {option});
+    markOptionSelected(option);
+}));
 
 (e => {
     const { option } = get('option', {option: 1});
-    document.querySelector(`#option-${option}`).setAttribute('checked', true);
     markOptionSelected(option);
-    console.log({option});
+    document.querySelector(`#option-${option}`).setAttribute('checked', true);
 })();
