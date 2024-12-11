2f97e9d162f15cfe4dea9726a0a9c9b04b136413
Imran Kabir <imran.kabir.k@gmail.com>
2024-12-11 12:35:27 +0500

    manners auto commit on 2024-12-11 12:35:27 PM

---
+++ README.md
@@ -0,0 +1,3 @@
+# You can use it on the following link
+
+Click to use [Kids App](https://imrankabir.github.io/kids)
---
+++ script.js
@@ -0,0 +1,26 @@
+const options = document.getElementsByName('option');
+const container = document.querySelector('.container');
+
+const get = (k, d) => JSON.parse(localStorage.getItem(`kids-${k}`)) ?? d;
+const set = (k, v) => localStorage.setItem(`kids-${k}`, JSON.stringify(v));
+
+const changeOption = e => {
+    const option = e.target.value;
+    console.log({option});
+    set('option', {option});
+    markOptionSelected(option);
+};
+
+const markOptionSelected = option => {
+    document.querySelectorAll('.option').forEach(option => option.classList.add('hide'));
+    document.querySelector(`.option-${option}`).classList.remove('hide');
+};
+
+options.forEach(option => option.addEventListener('click', changeOption));
+
+(e => {
+    const { option } = get('option', {option: 1});
+    document.querySelector(`#option-${option}`).setAttribute('checked', true);
+    markOptionSelected(option);
+    console.log({option});
+})();
